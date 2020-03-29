20c195907d429784150c141aa01df8aa7fa3f241
huhaiqng <1321385590@qq.com>
2020-03-29 18:04:05 +0800

    commit

--- vue.config.js
+++ vue.config.js
@@ -38,7 +38,7 @@ module.exports = {
     },
     proxy: {
       '/api': {
-        target: 'http://192.168.1.18:8000',
+        target: 'http://127.0.0.1:8000',
         changeOrigin: true
       }
     }
