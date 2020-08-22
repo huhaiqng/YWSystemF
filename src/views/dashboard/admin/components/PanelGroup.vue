eed7dec0a28584582e6d7273d1b75091d9ab5a71
huhaiqing <lixin@gainhon.com>
2020-08-22 17:03:31 +0800

    commit

--- src/views/dashboard/admin/components/PanelGroup.vue
+++ src/views/dashboard/admin/components/PanelGroup.vue
@@ -1,7 +1,7 @@
 <template>
   <el-row :gutter="40" class="panel-group">
     <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
-      <div class="card-panel" @click="toUrl('/resource/project')">
+      <div class="card-panel" @click="toUrl('/resource/index')">
         <div class="card-panel-icon-wrapper icon-shopping">
           <svg-icon icon-class="chart" class-name="card-panel-icon" />
         </div>
